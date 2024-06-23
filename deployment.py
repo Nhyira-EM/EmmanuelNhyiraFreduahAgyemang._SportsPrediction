8b0ad6d61a39bcf81ea30d10299f6f4bf9868a03
Nhyira <efreduahagyemang@gmail.com>
2024-06-23 20:50:54 +0000

    Update

--- deployment.py
+++ deployment.py
@@ -11,9 +11,9 @@ model = "C:/Users/Ato/OneDrive - Ashesi University/ASHESI/CS MAJOR/INTRO TO AI/L
 md = pickle.load(open(model, 'rb'))
 
 potential = st.number_input('Player Potential out of 100')
-value = st.number_input('Player Value in Euros')
-wage = st.number_input('Player Wage in Euros')
-release_clause = st.number_input('Player Release Clause in Euros')
+value_eur = st.number_input('Player Value in Euros')
+wage_eur = st.number_input('Player Wage in Euros')
+movement_reactions = st.number_input('Player Movement reactions')
 shot_power = st.number_input('Shot Power out of 100')
 passing = st.number_input('Passing out of 100')
 dribbling = st.number_input('Dribbling out of 100')
@@ -21,8 +21,8 @@ movement = st.number_input('Movement out of 100')
 mentality = st.number_input('Mentality out of 100')
 
 prediction = md.predict([[potential,
-value,
-wage,
+value_eur,
+wage_eur,
 release_clause,
 shot_power,
 passing,
